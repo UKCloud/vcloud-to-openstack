1247836fdf440629b236b1afb8a5a88e67917723
Rob Coward <rob@jive-videos.net>
2016-12-07 00:49:07 +0000

    Use git pull to refresh an existing checkout

--- files/fetch-scripts.sh
+++ files/fetch-scripts.sh
@@ -7,7 +7,13 @@ gem install rest-client
 gem install xml-simple
 
 cd ~/
-git clone $git_url vcloud-to-openstack
+if [ -d vcloud-to-openstack ]; then
+    cd vcloud-to-openstack
+    git pull
+    cd ..
+else
+    git clone $git_url vcloud-to-openstack
+fi
 
 chmod u+x vcloud-to-openstack/files/fetch-vm-list.rb
 vcloud-to-openstack/files/fetch-vm-list.rb > ~/vmlist.yaml
