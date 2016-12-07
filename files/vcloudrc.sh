2f952ae3a84baf780fcd7f12089fcff8844e190f
Rob Coward <rob@jive-videos.net>
2016-12-07 14:23:17 +0000

    Pass through openstack creds

--- files/vcloudrc.sh
+++ files/vcloudrc.sh
@@ -6,4 +6,8 @@ export VCD_USERID=$vcd_user
 export VCD_PASSWORD='$vcd_password'
 export VCD_VDC="$vcd_vdc"
 export VCD_URL=$vcd_url
+export OS_AUTH_URL=$os_auth_url
+export OS_PASSWORD='$os_password'
+export OS_PROJECT_NAME=$os_project_name
+export OS_USERNAME=$os_username
 EOF
