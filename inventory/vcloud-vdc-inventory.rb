8278c6d2a9d05a671ad915bd3617b95f15669e1a
Rob Coward <rob@jive-videos.net>
2016-12-15 13:44:30 +0000

    Add CentOS hostgroup, added hostvars for vapp name and storage requirements

--- inventory/vcloud-vdc-inventory.rb
+++ inventory/vcloud-vdc-inventory.rb
@@ -36,6 +36,7 @@ inventory = {}
 vapp_list = []
 vm_vars = {}
 windows_vms = []
+centos_vms = []
 
 parsed['VAppRecord'].each do |vapp|
   begin
@@ -49,11 +50,23 @@ parsed['VAppRecord'].each do |vapp|
   vapp_list << vapp_details['name']
   vapp_vms = []
   vapp_details['Children'][0]['Vm'].each do |vm|
+    # puts vm.to_json
     if vm['deployed'] == 'true' && vm['NetworkConnectionSection'][0]['NetworkConnection'][0]['IsConnected'][0] == 'true' then
       vapp_vms << vm['name']
       windows_vms << vm['name'] if vm['OperatingSystemSection'][0]['Description'][0] =~ /Windows/
+      centos_vms << vm['name'] if vm['OperatingSystemSection'][0]['Description'][0] =~ /CentOS/
+
+      storage = 0
+      vm['VirtualHardwareSection'][0]['Item'].each do |item|
+        if item['Description'][0] == "Hard disk" then
+          storage += item['HostResource'][0]['vcloud:capacity'].to_i
+        end
+      end
       vm_vars[vm['name']] = { 'ansible_host' => vm['NetworkConnectionSection'][0]['NetworkConnection'][0]['IpAddress'][0],
-                              'operating_system' => vm['OperatingSystemSection'][0]['Description'][0] }
+                              'operating_system' => vm['OperatingSystemSection'][0]['Description'][0],
+                              'storageGB' => (storage / 1024),
+                              'vapp' => vapp_details['name'],
+                              'vapp_storage' => (vapp['storageKB'].to_i / 1024 / 1024) }
     end
   end
 
@@ -64,6 +77,7 @@ inventory['vcloud_vms'] = { 'children' => vapp_list }
 inventory['_meta'] = { 'hostvars' => vm_vars }
 inventory['windows_vms'] = { 'hosts' => windows_vms,
                              'vars' => { 'ansible_connection' => 'winrm' }}
+inventory['centos_vms'] = { 'hosts' => centos_vms }
 
 opts.each do |opt, arg|
   case opt
